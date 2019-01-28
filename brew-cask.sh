fae3246afe00f0c3a47952395b7a9b6ed11d510a
Hiroyuki Nikaido <hiroyuki.nikaido@gmail.com>
2019-01-28 22:31:20 +0900

    Update brew-cask.sh

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
-brew cask install google-chrome
-brew cask install bettertouchtool
 brew cask install google-japanese-ime
-brew cask install slack
+
 brew cask install iterm2
-brew cask install postman
-brew cask install visual-studio-code
-brew cask install atom
+brew cask install bettertouchtool
 brew cask install sequel-pro
-brew cask install android-studio
 brew cask install keybase
-brew cask install zeplin
+brew cask install istat-menus
+
+brew cask install google-chrome
+brew cask install slack
+brew cask install postman
+
+brew cask install atom
+brew cask install visual-studio-code
+brew cask install android-studio
+brew cask install ruby-mine
+
 brew cask install sketch
 brew cask install sketch-toolbox
+brew cask install zeplin
+
 brew cask install adobe-creative-cloud
 open /usr/local/Caskroom/adobe-creative-cloud/latest/Creative\ Cloud\ Installer.app
